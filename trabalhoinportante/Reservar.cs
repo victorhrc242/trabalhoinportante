4d7fb45eceee16aa9c2809750795b0483ef268b5
Victorhugorodriguescost <163910434+victorhrc242@users.noreply.github.com>
2024-07-19 17:17:48 -0300

    ultimo ajuste

--- trabalhoinportante/Reservar.cs
+++ trabalhoinportante/Reservar.cs
@@ -9,20 +9,34 @@ namespace trabalhoinportante
 {
     public class Reservar
     {
+        recebercupom r = new recebercupom();
         public string acao { get; set; }
+        double numerocar=60;
+        double numeromoto=30;
         public void MenuReser()
         {
 
             Console.WriteLine("qual o seu veiculo,carro ou moto?");
+            acao = Console.ReadLine();
             if (acao=="carro")
             {
-                string veiculo = Console.ReadLine();
                 Console.WriteLine("qual a sua placa");
                 string placa = Console.ReadLine();
-                Console.WriteLine("");
+                Console.WriteLine("qual o seu cpf?");
+                double cpf = double.Parse(Console.ReadLine()); Console.WriteLine("vc quer usar o seu cupon?s/n");
+
+                if (acao == "s")
+                {
+
+                }
+                else if (acao == "n")
+                {
+                    Console.WriteLine($"vc ira pagar{numerocar}");
+                }
             }
             else if (acao=="moto")
             {
+                DateTime houra;
                 string veiculo = Console.ReadLine();
                 Console.WriteLine("qual a sua placa");
                 string placa = Console.ReadLine();
@@ -32,22 +46,14 @@ namespace trabalhoinportante
                 
                 if (acao=="s")
                 {
-               
+              
                 }
                 else if (acao == "n")
                 {
-
+                    Console.WriteLine($"vc ira pagar{numeromoto}");
                 }
-
-                
             }
-
-           
-
-
     }
-
-
     }
 }
 
